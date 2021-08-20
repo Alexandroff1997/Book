75013507021215fa0076e67a33bc51e59addf71f
Alex <alexandroff1997@gmail.com>
2021-08-20 20:34:03 +0700

    create state and didmount fetch api

--- src/App.js
+++ src/App.js
@@ -1,13 +1,42 @@
-import React from 'react'
+import React, {Component} from 'react'
 import Container from '@material-ui/core/Container'
-import {NavBar} from "./components/NavBar"
+import {NavBar} from "./components/NavBar/NavBar"
 
-function App() {
-  return (
-    <Container>
-      <NavBar />
-    </Container>
-  )
+export default class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      users: [],
+      isLoading: true,
+      sorted: false,
+    }
+  }
+
+  async componentDidMount() {
+    let users = localStorage.getItem('users')
+    try {
+      users = JSON.parse(users);
+    } catch (err) {
+      console.log('Users was not found')
+    }
+    if (!Array.isArray(users) || !users.length) {
+      users = await fetch('https://demo.sibers.com/users')
+        .then(response => response.json())
+      localStorage.setItem('users', JSON.stringify(users))
+    }
+    this.setState(state => ({
+      ...state,
+      users,
+      isLoading: false,
+    }))
+  }
+
+  render() {
+    return (
+      <Container>
+        <NavBar />
+      </Container>
+    )
+  }
 }
 
-export default App
