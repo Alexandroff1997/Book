f32d0dfa4eeb17c70752f827736742b4b3144161
Alex <alexandroff1997@gmail.com>
2021-08-20 15:35:33 +0700

    create navBar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import './App.css'
+import React from 'react'
+import Container from '@material-ui/core/Container'
+import {NavBar} from "./components/NavBar"
 
 function App() {
   return (
-    <div className="App">
-
-    </div>
+    <Container>
+      <NavBar />
+    </Container>
   )
 }
 
